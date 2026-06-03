f787db95db7188fe62230d55bbf73bcc563f5801
NamburiVivek <viveknamburiviveknamburi@gmail.com>
2026-06-03 12:28:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -30,10 +30,16 @@ app.use(express.static(path.join(__dirname)));
 
 // ── DB Pool ──────────────────────────────────────────────────
 const db = mysql.createPool({
-  host:     process.env.DB_HOST,
-  user:     process.env.DB_USER,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+
+  ssl: {
+    rejectUnauthorized: false
+  },
+
   waitForConnections: true,
   connectionLimit: 10,
 });
