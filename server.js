95d7b19435647f35f84606db07fa34555bc909fa
NamburiVivek <viveknamburiviveknamburi@gmail.com>
2026-06-06 22:22:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -34,6 +34,8 @@ const db = mysql.createPool({
   user:     process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port:     parseInt(process.env.DB_PORT) || 24711,
+  ssl:      { rejectUnauthorized: false },
   waitForConnections: true,
   connectionLimit: 10,
 });
